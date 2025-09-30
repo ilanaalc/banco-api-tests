d739bfa34117f251a23aa1863e2c72a096386aa4
Ilana Alcantara <ilanaalcantara4@gmail.com>
2025-09-30 08:54:20 -0300

    Adicionando a biblioteca .env para variváveis globais

--- test/login.test.js
+++ test/login.test.js
@@ -1,12 +1,13 @@
 const request = require('supertest');
 const { expect } = require('chai');
+require('dotenv').config();
 
 describe ('Login', () => {   
     describe ('POST /login', () => { //todos os testes estão relacionados ao POST /login
         it ('Deve retornar 200 com um token em string quando usar credenciais válidas', async () => { //usa o supertest para fazer uma requisição para uma api passando uma credencial válida
             
             //Faz uma requisição com o supertest via POST com credenciais validas para o POST /login
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
 
             //Define o método que vai ser usado e indentifica o endpoint
                 .post('/login')
--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,11 +1,12 @@
 const request = require('supertest');
 const { expect } = require('chai');
+require('dotenv').config();
 
 describe ('Transferênciais', () => {
     describe ('POST /transferênciais', () => {
         it ('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$10,00', async () => {
             //Capturar o token
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -15,7 +16,7 @@ describe ('Transferênciais', () => {
 
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
             .post('/transferencias')
             .set('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${token}`)
@@ -32,7 +33,7 @@ describe ('Transferênciais', () => {
 
         it ('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$10,00', async () => {
              //Capturar o token
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -42,7 +43,7 @@ describe ('Transferênciais', () => {
 
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
             .post('/transferencias')
             .set('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${token}`)
